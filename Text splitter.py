e2223a1bb983585502bb6a06a3ba0bb61bbc5a4e
Medenor <raphael-facchini@outlook.com>
2023-05-07 21:07:54 +0200

    Source added

--- Text splitter.py
+++ Text splitter.py
@@ -1,3 +1,5 @@
+# Origin: https://jnccxxkj.pakasak.com/python-python-split-string-in-parts-of-max-n-character-code-example
+
 line = "Text to Split"
 n = 2 #Number of character after which split occurs
 [line[i:i+n] for i in range(0, len(line), n)]
